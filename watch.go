62217724355485dd1e5029d402e4786372518823
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-10-18 09:56:24 +0200

    feat: Add watch function.

---
+++ watch.go
@@ -0,0 +1,53 @@
+// Copyright 2022 Jonas Kwiedor. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package baraddur
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"sync"
+	"time"
+)
+
+// Watch is to check the mod time of the different
+// files via wait groups. When a file receives a new
+// modification time, the function from the configuration struct
+// is executed. Also, can define the interval for each file.
+func (c *Config) Watch() error {
+
+	var routines sync.WaitGroup
+	routines.Add(reflect.ValueOf(c.Files).Len())
+
+	defer routines.Wait()
+
+	for _, value := range c.Files {
+
+		go func(path string, interval time.Duration, execute func()) {
+
+			defer routines.Done()
+
+			var modification time.Time
+			for range time.Tick(interval) {
+
+				stat, err := os.Stat(path)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				if !modification.IsZero() && modification.Before(stat.ModTime()) {
+					execute()
+				}
+				modification = stat.ModTime()
+
+			}
+
+		}(value.Path, value.Interval, value.Execute)
+
+	}
+
+	return nil
+
+}

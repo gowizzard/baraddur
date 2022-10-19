6e09ffd12c8d8dd7861693fedbecffe9249737d6
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-10-19 09:26:19 +0200

    test: Update test function & add multiple test files.

--- watch_test.go
+++ watch_test.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"sync"
 	"testing"
 	"time"
@@ -18,18 +19,31 @@ import (
 func TestWatch(t *testing.T) {
 
 	tests := []struct {
-		path   string
-		data   []byte
-		update []byte
-		perm   os.FileMode
+		files []struct {
+			path   string
+			data   []byte
+			update []byte
+			perm   os.FileMode
+			wait   time.Duration
+		}
 		config baraddur.Config
-		wait   time.Duration
 	}{
 		{
-			path:   filepath.Join(os.TempDir(), ".env"),
-			data:   []byte("PATH=~/usr/local/bin"),
-			update: []byte("PATH=~/usr/bin"),
-			perm:   0666,
+			files: []struct {
+				path   string
+				data   []byte
+				update []byte
+				perm   os.FileMode
+				wait   time.Duration
+			}{
+				{
+					path:   filepath.Join(os.TempDir(), ".env"),
+					data:   []byte("PATH=~/usr/local/bin"),
+					update: []byte("PATH=~/usr/bin"),
+					perm:   0666,
+					wait:   3 * time.Second,
+				},
+			},
 			config: baraddur.Config{
 				Files: []baraddur.File{
 					{
@@ -44,13 +58,30 @@ func TestWatch(t *testing.T) {
 					},
 				},
 			},
-			wait: 3 * time.Second,
 		},
 		{
-			path:   filepath.Join(os.TempDir(), "hello.txt"),
-			data:   []byte("This is a tests file."),
-			update: []byte("This is a updated test file."),
-			perm:   0666,
+			files: []struct {
+				path   string
+				data   []byte
+				update []byte
+				perm   os.FileMode
+				wait   time.Duration
+			}{
+				{
+					path:   filepath.Join(os.TempDir(), "hello.txt"),
+					data:   []byte("This is a tests file."),
+					update: []byte("This is a updated test file."),
+					perm:   0666,
+					wait:   15 * time.Second,
+				},
+				{
+					path:   filepath.Join(os.TempDir(), "bye.txt"),
+					data:   []byte("This is a tests file."),
+					update: []byte("This is a updated test file."),
+					perm:   0666,
+					wait:   6 * time.Second,
+				},
+			},
 			config: baraddur.Config{
 				Files: []baraddur.File{
 					{
@@ -63,15 +94,49 @@ func TestWatch(t *testing.T) {
 							t.Log("The file \"hello.txt\" was updated.")
 						},
 					},
+					{
+						Path:     filepath.Join(os.TempDir(), "bye.txt"),
+						Interval: 2 * time.Second,
+						Fault: func(err error) {
+							t.Log(err)
+						},
+						Execute: func() {
+							t.Log("The file \"bye.txt\" was updated.")
+						},
+					},
 				},
 			},
-			wait: 15 * time.Second,
 		},
 		{
-			path:   filepath.Join(os.TempDir(), "error.log"),
-			data:   []byte("ERROR[2022-08-26T11:51:30] This is a error message.\tFILE=log_test.go:65"),
-			update: []byte("ERROR[2022-08-26T11:52:00]  This is a updated error message.\tFILE=log_test.go:65"),
-			perm:   0666,
+			files: []struct {
+				path   string
+				data   []byte
+				update []byte
+				perm   os.FileMode
+				wait   time.Duration
+			}{
+				{
+					path:   filepath.Join(os.TempDir(), "error.log"),
+					data:   []byte("ERROR[2022-10-10T11:15:00] This is a error message.\tFILE=log_test.go:65"),
+					update: []byte("ERROR[2022-10-10T11:30:00]  This is a updated error message.\tFILE=log_test.go:65"),
+					perm:   0666,
+					wait:   30 * time.Second,
+				},
+				{
+					path:   filepath.Join(os.TempDir(), "warning.log"),
+					data:   []byte("WARNING[2022-10-10T10:30:00] This is a warning message.\tFILE=log_test.go:65"),
+					update: []byte("WARNING[2022-10-10T10:45:00] This is a updated warning message.\tFILE=log_test.go:65"),
+					perm:   0666,
+					wait:   15 * time.Second,
+				},
+				{
+					path:   filepath.Join(os.TempDir(), "information.log"),
+					data:   []byte("INFO[2022-10-10T10:45:00] This is a information message.\tFILE=log_test.go:65"),
+					update: []byte("INFO[2022-10-10T11:00:00] This is a updated information message.\tFILE=log_test.go:65"),
+					perm:   0666,
+					wait:   9 * time.Second,
+				},
+			},
 			config: baraddur.Config{
 				Files: []baraddur.File{
 					{
@@ -84,35 +149,58 @@ func TestWatch(t *testing.T) {
 							t.Log("The file \"error.log\" was updated.")
 						},
 					},
+					{
+						Path:     filepath.Join(os.TempDir(), "warning.log"),
+						Interval: 5 * time.Second,
+						Fault: func(err error) {
+							t.Log(err)
+						},
+						Execute: func() {
+							t.Log("The file \"warning.log\" was updated.")
+						},
+					},
+					{
+						Path:     filepath.Join(os.TempDir(), "information.log"),
+						Interval: 3 * time.Second,
+						Fault: func(err error) {
+							t.Log(err)
+						},
+						Execute: func() {
+							t.Log("The file \"information.log\" was updated.")
+						},
+					},
 				},
 			},
-			wait: 30 * time.Second,
 		},
 	}
 
 	for _, value := range tests {
 
-		err := os.WriteFile(value.path, value.data, value.perm)
-		if err != nil {
-			log.Fatal(err)
+		for _, value := range value.files {
+			err := os.WriteFile(value.path, value.data, value.perm)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 
 		var routines sync.WaitGroup
-		routines.Add(2)
+		routines.Add(reflect.ValueOf(value.files).Len() + 1)
 
 		go func(c baraddur.Config) {
 			defer routines.Done()
 			c.Watch()
 		}(value.config)
 
-		go func(wait time.Duration, path string, update []byte, perm os.FileMode) {
-			defer routines.Done()
-			time.Sleep(wait)
-			err := os.WriteFile(path, update, perm)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(value.wait, value.path, value.update, value.perm)
+		for _, value := range value.files {
+			go func(wait time.Duration, path string, update []byte, perm os.FileMode) {
+				defer routines.Done()
+				time.Sleep(wait)
+				err := os.WriteFile(path, update, perm)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}(value.wait, value.path, value.update, value.perm)
+		}
 
 		routines.Wait()
 
